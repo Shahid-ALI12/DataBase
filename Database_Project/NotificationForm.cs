3434c9ef942f2c887cc0dca0597ca45167fbbee7
Shahid Ali <shahidshafaqat2007@gmail.com>
2026-05-21 07:11:25 +0500

    Add files via upload

---
+++ Database_Project/NotificationForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database_Project
+{
+    public partial class NotificationForm : Form
+    {
+        public NotificationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NotificationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
